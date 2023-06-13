221d6b0dc5ee7ca14ac65f79cfcdf2d0b39b5469
zudo <44712315+zudo@users.noreply.github.com>
2023-06-13 20:44:23 +0200

    revert

--- src/lib.rs
+++ src/lib.rs
@@ -145,15 +145,8 @@ impl Images {
         ))
     }
     pub fn new<Hash: Digest<OutputSize = U64>>(secrets: &[Secret]) -> Images {
-        Images(
-            secrets
-                .iter()
-                .map(|secret| {
-                    let a = secrets[0].0 * RISTRETTO_BASEPOINT_POINT;
-                    let b = point::hash::<Hash>(a);
-                    secret.0 * b
-                })
-                .collect(),
-        )
+        let a = secrets[0].0 * RISTRETTO_BASEPOINT_POINT;
+        let b = point::hash::<Hash>(a);
+        Images(secrets.iter().map(|x| x.0 * b).collect())
     }
 }
